8db0b4154b7f63980ee8bb373a0648a69d926923
TheAkuma010 <torresgabriel.gt10@gmail.com>
2026-08-18 20:18:59 -0300

    feat: adiciona serviços IClientService e IProductService ao contêiner de injeção de dependência

--- backend/Sales.Api/Program.cs
+++ backend/Sales.Api/Program.cs
@@ -1,4 +1,5 @@
 using Sales.Application.Interfaces;
+using Sales.Application.Services;
 using Sales.Infrastructure.Database;
 using Sales.Infrastructure.Repositories;
 
@@ -14,6 +15,9 @@ builder.Services.AddSingleton<IDbConnectionFactory>(
 
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+builder.Services.AddScoped<IClientService, ClientService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
