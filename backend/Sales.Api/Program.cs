8ff15af3948da411dae8b5e7dbe317013ada8c69
TheAkuma010 <torresgabriel.gt10@gmail.com>
2026-08-18 20:45:28 -0300

    feat: removendo endpoint de exemplo e certificando o uso do middleware de exceções

--- backend/Sales.Api/Program.cs
+++ backend/Sales.Api/Program.cs
@@ -1,3 +1,4 @@
+using Sales.Api.Middleware;
 using Sales.Application.Interfaces;
 using Sales.Application.Services;
 using Sales.Infrastructure.Database;
@@ -33,31 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
