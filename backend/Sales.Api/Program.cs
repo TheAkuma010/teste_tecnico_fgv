4bb66601fb6cdc9dc3146cdcddc99ee078977334
TheAkuma010 <torresgabriel.gt10@gmail.com>
2026-08-18 19:56:06 -0300

    feat: adiciona configuração de conexão com o banco de dados e repositório de produtos

--- backend/Sales.Api/Program.cs
+++ backend/Sales.Api/Program.cs
@@ -1,4 +1,19 @@
+using Sales.Application.Interfaces;
+using Sales.Infrastructure.Database;
+using Sales.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString =
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException(
+        "Connection string 'DefaultConnection' was not configured.");
+
+builder.Services.AddSingleton<IDbConnectionFactory>(
+    new SqlConnectionFactory(connectionString));
+
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
